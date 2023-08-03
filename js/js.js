281b39aef1a53772dcdd4348baedba3b3b60b362
Dory Nehme <doryy.nehme@gmail.com>
2023-08-03 11:29:10 +0300

    animation statistics

--- js/js.js
+++ js/js.js
@@ -16,3 +16,54 @@ function PinkSeperatorAnim() {
 }
 
 window.addEventListener("scroll", PinkSeperatorAnim);
+
+
+
+
+
+/**
+ Counting in statistics
+ */
+
+ $(document).ready(function() {
+  $(window).scroll(function() {
+    // Get the top position of the row element
+    var rowTopPosition = $('.row').offset().top;
+    var windowHeight = $(window).height();
+    var scrollTop = $(window).scrollTop();
+
+    // Check if the row is in the viewport
+    if (scrollTop + windowHeight > rowTopPosition) {
+      // Start the counting animation for MyServicesNumber
+      $({ countNum: 0 }).animate({
+        countNum: 12 // Change this to your desired number
+      }, {
+        duration: 3000, // 3 seconds for the counting animation
+        easing: 'linear',
+        step: function() {
+          $('#MyServicesNumber').text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $('#MyServicesNumber').text(this.countNum);
+        }
+      });
+
+      // Start the counting animation for RegisteredCompaniesNumber
+      $({ countNum: 0 }).animate({
+        countNum: 400 // Change this to your desired number
+      }, {
+        duration: 3000, // 3 seconds for the counting animation
+        easing: 'linear',
+        step: function() {
+          $('#RegisteredCompaniesNumber').text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $('#RegisteredCompaniesNumber').text(this.countNum);
+        }
+      });
+
+      // Unbind the scroll event once the counting animations are triggered
+      $(window).off('scroll');
+    }
+  });
+});
